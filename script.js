4b0709280a716283dc57a32e488032fd252b2558
Gabriel marques do nascimento <gabrielmarquesnascimentob@gmail.com>
2024-10-24 12:45:46 -0300

    upp

--- script.js
+++ script.js
@@ -1,91 +1,197 @@
+// barra que recebe o texto que o usuario quer ler
+const input = document.getElementById("ler");
+// botonhes para mostrar e esconder o texto
+const startBTN = document.getElementById("start");
+const resetBTN = document.getElementById("reset");
+// elemento que mostra o texto
+const paragrafo = document.getElementById("paragraph");
+// botao responcavel por scroll's o equivalente a uma pagina
+const page = document.getElementById("page");
+// velocidade de scroll
+const scrollspeed = 1;
+// largura da janela
+const width = window.innerWidth;
+// altura da janela
+const height = window.innerHeight;
+// botonhes responcaveis por mover o marcador para cima ou para baixo
+let setaps = document.getElementById("setas");
+let setapb = document.getElementById("setab");
 
+// Criar o elemento de destaque o marcador de texto
+const highlight = document.createElement("div");
+// add uma classe com os estilos para o marcador
+highlight.className = "highlight";
+// mostra os estilos utilizados no marcador
+const highlight_estilo = window.getComputedStyle(highlight);
+// add o marcador ao elemento pai body
+document.body.appendChild(highlight);
+// armazeana a posicao top do elemento para usos futuros
+let highlight_top_erd = highlight_estilo.top;
 
+// funcao responcavel por scrollar ate a distancia em pixels especificada
+function scrollarParagrafo(pixels) {
+  // paragrafo.scrollTo({
+  //   top: pixels - 55,
+  //   behavior: "smooth",
+  // });
+  paragrafo.scrollTo({
+    top: pixels,
+    behavior: "smooth",
+  });
+}
 
-      const input = document.getElementById("ler");
-      const startBTN = document.getElementById("start");
-      const resetBTN = document.getElementById("reset");
-      const paragrafo = document.getElementById("paragraph");
-      const page = document.getElementById("page");
+// define o comportamento de alguns elementos como
+// deve aparecer ou desaparecer da tela
+function style_sec(rstdis, pdis, stdis, inpdis, texto) {
+  resetBTN.style.display = rstdis;
+  paragrafo.style.display = pdis; // Faz o parágrafo aparecer
+  startBTN.style.display = stdis;
+  input.style.display = inpdis;
+  paragrafo.innerHTML = texto;
+}
 
-      const scrollspeed = 1;
-      const width = window.innerWidth; // largura da janela
-      const height = window.innerHeight; // altura da janela
+// retorna uma lista de elementos
+let buttons = document.querySelectorAll(".scrollbt");
+// funcso que define se os bottons devem aparecer ou ficar escondido
+function buttonstatic(estado) {
+  // loop
+  buttons.forEach((button) => {
+    button.style.display = estado;
+  });
+}
 
-      function scrollarParagrafo(pixels) {
-        paragrafo.scrollTo({
-          top: pixels - 55,
-          behavior: "smooth",
-        });
-      }
+// apos add o texto ao input penas com um enter ele mostra o texto
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    // Aqui você pode colocar a ação que deseja realizar ao pressionar Enter
+    saveText(input.value, "savedText");
+    buttonstatic("block");
+    style_sec("block", "block", "none", "none", input.value);
+  }
+});
+// apos add o texto ao input penas com um click no 'start' ele mostra o texto
+startBTN.addEventListener("click", () => {
+  saveText(input.value, "savedText");
+  style_sec("block", "block", "none", "none", input.value);
+  buttonstatic("block");
+});
+// apos add o texto ao input penas com um 'reset' ele esconde  o texto
+// para add um novo texto
+resetBTN.addEventListener("click", () => {
+  buttonstatic("none");
+  style_sec("none", "none", "block", "block", "");
+});
 
-      function style_sec(rstdis, pdis, stdis, inpdis, texto) {
-        resetBTN.style.display = rstdis;
-        paragrafo.style.display = pdis; // Faz o parágrafo aparecer
-        startBTN.style.display = stdis;
-        input.style.display = inpdis;
-        paragrafo.innerHTML = texto;
-      }
+// apos clicar em um botão com formato de livro
+// salta o equivalente a uma pagina
+let pixels = 0;
 
+page.addEventListener("click", () => {
+  // posicao em pixels do scroll
+  const scrolltop = paragrafo.scrollTop;
+  // pega a posicao de pixels do scroll mais a altura da janela
+  heightp = Number(paragrafo_style.height.replace('px', ''))
+  pixels = heightp + scrolltop;
+  console.log("pixels", pixels, "height", heightp);
 
-     
-      let buttons = document.querySelectorAll(".scrollbt");
-      // funcso que define se os bottons devem aparecer ou ficar escondido
-      function buttonstatic(estado)
-      {
-        for (let i = 0; i < buttons.length; i++)
-        {
-          buttons[i].style.display = estado
-        }
-      }
-      
-      
-      input.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-          // Aqui você pode colocar a ação que deseja realizar ao pressionar Enter
-          saveText(input.value,"savedText");
-          buttonstatic("block")
-          style_sec("block", "block", "none", "none", input.value);
-        }
-      });
+  scrollarParagrafo(pixels);
+  let teste = highlight_top_erd;
+  console.log("paragrafo_style", teste);
+  if (marcador.value == "button") {
 
-      startBTN.addEventListener("click", () => {
-        saveText(input.value,"savedText");
-        style_sec("block", "block", "none", "none", input.value);
-        buttonstatic("block")
+    highlight.style.top = highlight_top_erd;
+    highlight.style.transition = 'top 2s ease'; 
 
-      });
-      resetBTN.addEventListener("click", () => {
-        buttonstatic("none")
-        style_sec("none", "none", "block", "block", "");
-      
-      });
-      let pixels = 0;
+  }
+});
 
-      page.addEventListener("click", () => {
-        const scrolltop = paragrafo.scrollTop;
-        pixels = height + scrolltop;
-        console.log("pixels", pixels, "height", height);
-        scrollarParagrafo(pixels);
-      });
+// # ----------------------------------------------------------------------------------------------------------------------
+//                gernciamento de texto
+function saveText(data, name) {
+  // Pega o texto da área de texto
+  const text = data;
+  // Salva no LocalStorage
+  localStorage.setItem(name, text);
+}
 
+function loadText(name) {
+  // Carrega o texto salvo do LocalStorage, se existir
+  const savedText = localStorage.getItem(name);
+  if (savedText) {
+    style_sec("block", "block", "none", "none", savedText);
+    input.value = savedText;
+    return savedText;
+  }
+}
 
+// Chama a função para carregar o texto ao abrir a página
+window.onload = loadText("savedText");
 
-      function saveText(data, name) {
-        // Pega o texto da área de texto
-        const text = data;
-        // Salva no LocalStorage
-        localStorage.setItem(name, text);
-      }
+//----------------------------------------------------------------------------------------------------------------------
+//                   marcador de texto
+// Função para alterar o valor de 'top'
+let highlight_top = Number(highlight_estilo.top.replace("px", ""));
+function alterarTop(novoTop) {
+  let styleheight = Number(paragrafo_style.height.replace("px", ""));
+  console.log(novoTop, styleheight);
+  if (novoTop < highlight_top) {
+    novoTop = highlight_top;
+  }
+  if (novoTop > styleheight - 25) {
+    novoTop = styleheight - 10;
+  }
 
-      function loadText(name) {
-        // Carrega o texto salvo do LocalStorage, se existir
-        const savedText = localStorage.getItem(name);
-        if (savedText) {
-          style_sec("block", "block", "none", "none", savedText);
-          input.value = savedText;
-          return savedText
-        }
-      }
+  highlight.style.top = novoTop + "px";
+}
 
-      // Chama a função para carregar o texto ao abrir a página
-      window.onload = loadText("savedText");
+function recarregarPagina() {
+  location.reload();
+}
+
+function scrollarline(pixel) {
+  highlight.scrollTo({
+    top: pixel,
+    behavior: "smooth",
+  });
+}
+
+function move_marcador(line) {
+  // Pegue o valor do line-height
+  var linha = line
+    ? Number(paragrafo_style.lineHeight.replace("px", ""))
+    : -Number(paragrafo_style.lineHeight.replace("px", "")) ;
+
+  alterarTop(Number(highlight_estilo.top.replace("px", "")) + linha);
+  highlight.style.transition = 'top 0.1s ease';
+}
+
+let cont = 1;
+function highlight_status() {
+  if (marcador.value == "mouse") {
+    cont = 0;
+    setapb.disabled = true;
+    document.addEventListener("mousemove", function (event) {
+      const y = event.clientY;
+      alterarTop(y);
+    });
+
+    console.log(marcador.value);
+  }
+  if (marcador.value == "button") {
+    console.log(marcador.value);
+    if (cont < 1) {
+      cont++;
+      recarregarPagina();
+    }
+  }
+}
+
+setapb.addEventListener("click", () => {
+  console.log("seta para baixo");
+  move_marcador(true);
+});
+
+setaps.addEventListener("click", () => {
+  console.log("seta para baixo");
+  move_marcador(false);
+});

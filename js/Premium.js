b1437530a77af03a11954bb995ce61f18c509628
Gabriel marques do Nascimento <gabrielmarquesnascimentob@gmail.com>
2024-11-15 14:58:08 -0300

    premium logo

--- js/Premium.js
+++ js/Premium.js
@@ -1,21 +1,26 @@
 let logo = document.createElement("div");
-logo.innerText = "Premium"
+logo.innerText = "Premium";
+logo.translate = false;
 // --------- teste bet ------------
-const teste = document.getElementById("marcador");
+const teste = document.getElementById("bookmark");
 teste.classList.add("premiumitem");
 
 //-----------------------------------
+const menuSettings = document.getElementById("conteiner");
 const Premium = document.querySelectorAll(".premiumitem");
 
+conteiner.appendChild(logo)
 for (let i = 0; i < Premium.length; i++) {
     let cloneLogo = logo.cloneNode(true);
 
-    let style = window.getComputedStyle(Premium[i]);
-    for (let propety of style) {
-        cloneLogo.style[propety] = style.getPropertyValue(propety);
-    }
-    console.log(style);
-    document.body.appendChild(cloneLogo);
-    Premium[i].style.opacity = 0;
-    Premium[i].style.display = "none";
+    // let style = window.getComputedStyle(Premium[i]);
+//     for (let propety of style) {
+//         cloneLogo.style[propety] = style.getPropertyValue(propety);
+//     }
+//     console.log(style);
+//     menuSettings.appendChild(cloneLogo);
+    
+    Premium[i].disabled = true;
+    const label = document.querySelector(`label[for="${Premium[i].id}"]`);
+    label.classList.add("disabled-label")
 }

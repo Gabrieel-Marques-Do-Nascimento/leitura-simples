f5945fbc05dca95f7f7cea181fc6aa9203fc9bf7
Gabriel marques do nascimento <gabrielmarquesnascimentob@gmail.com>
2024-12-17 13:04:43 -0300

    Refactor profile.js: Add fetch for user and bookmark info, improve HTML ID handling

--- js/profile.js
+++ js/profile.js
@@ -1,10 +1,42 @@
 import { loadText_Cache_json } from "./global.js";
 
-
-let user = loadText_Cache_json("TOKEN");
-if ( user == null) {
+const user = loadText_Cache_json("TOKEN", null);
+const id= loadText_Cache_json("LITBOOKMARKID", null);
+console.log(id);
+console.log(user);
+if (user == null || id == null) {
      window.location.href = "login.html";
-}
-else {
-     document.getElementById("user").innerHTML = user.name;
+} else {
+     const username = document.getElementById("name");
+     // username.innerHTML = user.name;
+     const email = document.getElementById("email");
+     const genero = document.getElementById("genero");
+     const idioma = document.getElementById("idioma");
+     const plano = document.getElementById("plano");
+
+     // Fazer a requisição GET com um corpo JSON
+     fetch(`http://127.0.0.1:5000/modulo2/info`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${user}`,
+            uid: id
+          }
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error('There has been a problem with your fetch operation:', error);
+        });
+        
+
+
+        
 }

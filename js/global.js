569372255d1199739ac9118c13a66fb10e1e4063
Gabriel marques do nascimento <gabrielmarquesnascimentob@gmail.com>
2024-12-25 18:56:56 -0300

    updt

--- js/global.js
+++ js/global.js
@@ -1,5 +1,4 @@
-export url_api = "http://127.0.0.1:5000";
-
+export const url_api = "http://127.0.0.1:5000";
 
 /**
  * Carrega um objeto JSON armazenado no LocalStorage.
@@ -8,22 +7,22 @@ export url_api = "http://127.0.0.1:5000";
  * @param {string} name - A chave usada para recuperar o objeto do LocalStorage.
  * @returns {Object} O objeto JSON recuperado ou um objeto padrão caso não exista.
  */
-export function loadText_Cache_json(name,is_principal=true) {
+export function loadText_Cache_json(name, is_principal = true) {
      const text = localStorage.getItem(name);
      // Pega o texto do LocalStorage
      if (is_principal) {
-              const padrao = {
-          lang: "en",
-          delay: 2000,
-          markmove: "button",
-          "font-size": 18,
-          //background_type: "white",
-          theme: "auto",
-          $autoScroll: false,
-          color: "#00f000",
-          screentype: "markdow",
-     }; 
-     // Verifica se o item existe e converte de volta para um objeto
+          const padrao = {
+               lang: "en",
+               delay: 2000,
+               markmove: "button",
+               "font-size": 18,
+               //background_type: "white",
+               theme: "auto",
+               $autoScroll: false,
+               color: "#00f000",
+               screentype: "markdow",
+          };
+          // Verifica se o item existe e converte de volta para um objeto
           return text ? JSON.parse(text) : padrao;
      }
      return text ? JSON.parse(text) : null;
@@ -37,20 +36,20 @@ export function loadText_Cache_json(name,is_principal=true) {
  */
 
 export function save_json_to_cache(data, name) {
-    // Converte o objeto para uma string JSON
-    const text = JSON.stringify(data);
-    // Salva no LocalStorage
-    localStorage.setItem(name, text);
+     // Converte o objeto para uma string JSON
+     const text = JSON.stringify(data);
+     // Salva no LocalStorage
+     localStorage.setItem(name, text);
 }
 
 /**
- * 
+ *
  * @param {*} name ex: name: /moduloN/login
- * @returns 
+ * @returns
  */
 export async function requestGET(name) {
      url = url_api + name;
-  try {
+     try {
           const response = await fetch(url);
           if (!response.ok) {
                throw new Error(`Erro na solicitação: ${response.status}`);
